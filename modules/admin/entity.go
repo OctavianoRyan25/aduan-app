2b8034cfc0a5787b1978f8b920b5390d7caff4dc
OctavianoRyan25 <octavianoryan25@gmail.com>
2024-05-10 21:53:44 +0700

    bug-fix

--- modules/admin/entity.go
+++ modules/admin/entity.go
@@ -2,6 +2,8 @@ package admin
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type Admin struct {
@@ -11,4 +13,15 @@ type Admin struct {
 	Password   string
 	Created_at time.Time
 	Updated_at time.Time
+}
+type User struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Email      string
+	Password   string
+	Phone      string
+	Address    string
+	Created_at time.Time
+	Updated_at time.Time
+	Deleted_at gorm.DeletedAt
 }
